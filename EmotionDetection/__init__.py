6b23ebfe28bc28d9e0ee5dd71c9b416222dd75d5
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2025-08-26 14:18:24 +0100

    package the application

---
+++ EmotionDetection/__init__.py
@@ -0,0 +1,6 @@
+from . import emotion_detector
+
+'''To usepackage, from python shell, run:
+>>>from EmotionDetection.emotion_detection import emotion_detector    
+>>> emotion_detector('I hate working long hours.')
+{'anger': 0.64999825, 'disgust': 0.03349073, 'fear': 0.05567236, 'joy': 0.008661813, 'sadness': 0.19678932, 'dominant_emotion': 'anger'}'''
