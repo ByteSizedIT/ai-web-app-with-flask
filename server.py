ca839b87d8e5a178cdae91fb1807b87e7566c51f
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2025-08-26 19:37:04 +0100

    update following pyling recommendations:
    python3.11 -m pip install pylint
    pylint server.py
    
    sample output:
    theia@theiadocker-nkeemer:/home/project/final_project$ pylint server.py
    server.py:10:0: C0325: Unnecessary parens after 'if' keyword (superfluous-parens)
    server.py:19:0: C0304: Final newline missing (missing-final-newline)
    server.py:1:0: C0114: Missing module docstring (missing-module-docstring)
    server.py:7:0: C0116: Missing function or method docstring (missing-function-docstring)
    server.py:15:0: C0116: Missing function or method docstring (missing-function-docstring)
    
    ------------------------------------------------------------------
    Your code has been rated at 6.15/10 (previous run: 6.15/10, +0.00)
    
    theia@theiadocker-nkeemer:/home/project/final_project$ pylint server.py
    
    -------------------------------------------------------------------
    Your code has been rated at 10.00/10 (previous run: 6.15/10, +3.85)

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,14 +10,21 @@ app = Flask("__name__")
 
 @app.route("/emotionDetector")
 def sent_dectector():
+    '''receives the text from the HTML interface and 
+    runs emotion detection over it using emotion_detection()
+    function. The output returned shows scores for each emotion.
+'''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    if(response['dominant_emotion'] is None):
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
     return f"For the given statement, the system response is {str(response)[1:-1]}"
 
 @app.route("/")
 def render_index_page():
+    '''initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
